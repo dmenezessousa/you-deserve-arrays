9ccf74fe93b593387fa25af8db06163106066344
Diego Sousa <dmenezessousaa@gmail.com>
2021-06-15 12:12:07 -0400

    Update firstItemIsNumber.js

--- firstItemIsNumber.js
+++ firstItemIsNumber.js
@@ -11,28 +11,28 @@ count = 0;
 console.log(array);
 
 
-// if(array.length !== String){
-//     console.log("The first array is a number.");
-// }
-// // if(array.leagth !== String){
-// //     console.log("The fist array is not a number.");
-// // }
+if(array.length !== String){
+    console.log("The first array is a number.");
+}
+if(array.leagth !== String){
+    console.log("The fist array is not a number.");
+}
 
-// if(array.indexOf(0) == Number){
-//     console.log('not a number');
-// }
-// else if(array.indexOf(0) !== String){
-//     console.log('a number');
-// }
+if(array.indexOf(0) == Number){
+    console.log('not a number');
+}
+else if(array.indexOf(0) !== String){
+    console.log('a number');
+}
 
-// let number = array.includes(Number.array,0);
-// let string = array.includes(String.array,0);
-// console.log(array);
+let number = array.includes(Number.array,0);
+let string = array.includes(String.array,0);
+console.log(array);
 
 
-// if(array[0] == number){
-//     console.log("is a number")
-// }
-// if(array[0] == string){
-//     console.log("not a number");
-// }
+if(array[0] == number){
+    console.log("is a number")
+}
+if(array[0] == string){
+    console.log("not a number");
+}
